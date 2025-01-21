104208b80ade2c322a4f7d4ed2c1e6c616fc9e76
Umair-Ali <ali456umair123@gmail.com>
2025-01-22 01:22:13 +0500

    outside the folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './component/Button1'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -11,10 +12,10 @@ function App() {
       <div className='mb-4'>
         <h1>Hello World!</h1>
         <h2>Umair Ali</h2>
-
+        <Button/>
       </div>
     </>
   )
 }
-
+// Button
 export default App
---
+++ src/component/Button1.tsx
@@ -0,0 +1,5 @@
+const Button = ({text = "submit"}) => {
+    return <button>{text}</button>
+}
+
+export default Button
